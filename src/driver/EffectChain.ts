976b7ccabd91113d9525174ac6aac15367ba7841
omer_sm <smorodinskyomer@gmail.com>
2025-04-13 14:11:59 +0300

    feat(EffectChain.ts): add basic effect chain

---
+++ src/driver/EffectChain.ts
@@ -0,0 +1,74 @@
+import { Gain, ToneAudioNode, ToneAudioNodeOptions } from "tone"
+import { ToneWithContextOptions } from "tone/build/esm/core/context/ToneWithContext"
+import { Effect, EffectOptions } from "tone/build/esm/effect/Effect"
+
+export interface SynthEffect {
+    id: number | 'input' | 'output';
+    node: Effect<EffectOptions> | ToneAudioNode<ToneWithContextOptions>;
+    inputs: SynthEffect['id'][];
+    outputs: SynthEffect['id'][];
+}
+
+export default class EffectChain extends ToneAudioNode<ToneAudioNodeOptions> {
+    input: ToneAudioNode<ToneWithContextOptions>
+    output: ToneAudioNode<ToneWithContextOptions>
+    readonly name: string = 'EffectChain';
+    effects: Map<SynthEffect['id'], SynthEffect>;
+    effectIdCounter: number;
+
+    constructor(options: ToneAudioNodeOptions) {
+        super(options);
+        this.effectIdCounter = 0;
+        this.input = new Gain({context: this.context});
+        this.output = new Gain({context: this.context});
+        this.effects = new Map<SynthEffect['id'], SynthEffect>();
+        this.addEffect(this.input, 'input');
+        this.addEffect(this.output, 'output');
+
+        this.addConnection('input', 'output');
+    }
+
+    addEffect(node: SynthEffect['node'], id?: SynthEffect['id']): SynthEffect['id'] {
+        id = id ?? this.effectIdCounter++;
+        this.effects.set(id, {id, node, inputs: [], outputs: []});
+
+        return id;
+    }
+
+    addConnection(from: SynthEffect['id'], to: SynthEffect['id']): void {
+        const fromEffect = this.effects.get(from);
+        const toEffect = this.effects.get(to);
+
+        if (!fromEffect || !toEffect) {
+            throw new Error(`Invalid effect id: ${from} or ${to}`);
+        }
+
+        fromEffect.outputs.push(to);
+        toEffect.inputs.push(from);
+        fromEffect.node.connect(toEffect.node);
+    }
+
+    removeConnection(from: SynthEffect['id'], to: SynthEffect['id']): void {
+        const fromEffect = this.effects.get(from);
+        const toEffect = this.effects.get(to);
+
+        if (!fromEffect || !toEffect) {
+            throw new Error(`Invalid effect id: ${from} or ${to}`);
+        }
+
+        fromEffect.outputs = fromEffect.outputs.filter(output => output !== to);
+        toEffect.inputs = toEffect.inputs.filter(input => input !== from);
+        fromEffect.node.disconnect(toEffect.node);
+    }
+
+    dispose(): this {
+        this.input.dispose();
+        this.output.dispose();
+        this.effects.forEach(effect => {
+            effect.node.dispose();
+        });
+        this.effects.clear();
+        
+        return super.dispose();
+    }
+}
--- src/driver/driver.ts
+++ src/driver/driver.ts
@@ -1,5 +1,10 @@
+import { getContext } from "tone"
 import DriverSynth from "./DriverSynth"
+import EffectChain from "./effectChain"
 
-const driverSynth = new DriverSynth().toDestination();
+const driverSynth = new DriverSynth();
+const effectChain = new EffectChain({context: getContext()});
+driverSynth.connect(effectChain.input);
+effectChain.toDestination();
 
 export default driverSynth;
