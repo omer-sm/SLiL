a4eeddf17eb91b728f9eb0b66845b323e1483984
omer_sm <smorodinskyomer@gmail.com>
2025-04-13 12:54:12 +0300

    feat(melodies.ts): add preview melodies

---
+++ src/tabs/Preview/utils/melodyTypes.ts
@@ -0,0 +1,14 @@
+import { Note } from "tone/build/esm/core/type/NoteUnits"
+import { Time } from "tone/build/esm/core/type/Units"
+
+export interface MelodyNote {
+    note: Note;
+    duration: Time;
+    startTime: Time;
+}
+
+export interface Melody {
+    name: string;
+    bpm: number;
+    notes: MelodyNote[];
+}
