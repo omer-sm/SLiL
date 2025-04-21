fd978784841bb83ba87ab679641e259461f361f3
omer_sm <smorodinskyomer@gmail.com>
2025-04-21 09:11:24 +0300

    feat(MasterLfoController.tsx): change param select to dropdown

--- src/tabs/Synth/LFO/MasterLFOController.tsx
+++ src/tabs/Synth/LFO/MasterLFOController.tsx
@@ -1,22 +1,18 @@
 import { lfoState } from '../../../state/LFO/lfoState';
-import { Button, Card, Divider, Flex, Form, Input, Tabs } from 'antd';
+import { Button, Card, Divider, Flex, Form, Tabs, Select } from 'antd';
 import { useState } from 'react';
-import { isNumber } from 'tone';
 import SingleLfoController from './SingleLfoController';
 import { modulatablesState } from '../../../state/Modulatables/modulatablesState';
 import EffectChain from '../../../driver/EffectChain';
+import { useSnapshot } from 'valtio';
 
 export default function MasterLFOController() {
   const [currentLfo, setCurrentLfo] = useState<1 | 2 | 3>(1);
   const [newConnection, setNewConnection] = useState({ effectId: '', param: '' });
+  const modulatablesSnap = useSnapshot(modulatablesState);
 
   const addConnection = (lfoIndex: number) => {
     const { effectId, param } = newConnection;
-    if (effectId !== 'input' && effectId !== 'output' && !isNumber(+effectId)) {
-      setNewConnection({ effectId: '', param: '' });
-      throw new Error('Effect ID must be a number, "input", or "output"');
-    }
-
     if (effectId && param) {
       const lfoKey = `lfo${lfoIndex + 1}` as keyof typeof lfoState;
       lfoState[lfoKey].connections = [
@@ -48,20 +44,29 @@ export default function MasterLFOController() {
               <SingleLfoController lfoKey={`lfo${item.key}` as keyof typeof lfoState} />
               <Divider />
               <Form layout="inline" style={{}}>
-                <Form.Item label="Effect ID">
-                  <Input
-                    value={newConnection.effectId}
-                    onChange={(e) =>
-                      setNewConnection({ ...newConnection, effectId: e.target.value })
+                <Form.Item label="Effect and Param">
+                  <Select
+                    style={{ minWidth: '10rem' }}
+                    showSearch
+                    value={
+                      newConnection.effectId && newConnection.param
+                        ? `${newConnection.effectId}.${newConnection.param}`
+                        : ''
                     }
-                  />
-                </Form.Item>
-                <Form.Item label="Param">
-                  <Input
-                    value={newConnection.param}
-                    onChange={(e) =>
-                      setNewConnection({ ...newConnection, param: e.target.value })
-                    }
+                    onChange={(value) => {
+                      const [effectId, param] = value.split('.');
+                      setNewConnection({ effectId, param });
+                    }}
+                    options={Object.keys(modulatablesSnap.params).map((effectId) => ({
+                      label: `Effect #${effectId}`,
+                      options: Object.values(
+                        modulatablesSnap.params[EffectChain.parseId(effectId)] || {}
+                      ).map((param) => ({
+                        value: `${effectId}.${param.param}`,
+                        label: param.param,
+                        disabled: param.isModulated
+                      })),
+                    }))}
                   />
                 </Form.Item>
                 <Form.Item>
