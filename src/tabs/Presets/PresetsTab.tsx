b77c5b08d68452fee6018c2ce823ae35298877bd
omer_sm <smorodinskyomer@gmail.com>
2025-05-30 15:22:46 +0300

    feat(PresetsTab.tsx, usePreset.tsx): add preset saving

--- src/tabs/Presets/PresetsTab.tsx
+++ src/tabs/Presets/PresetsTab.tsx
@@ -1,21 +1,53 @@
-import { Button, List } from "antd";
-import { presets } from "./utils/presets";
-import { useLoadPreset } from "./utils/useLoadPreset"
+import { Button, Card, Divider, Form, Input, List } from 'antd';
+import { presets } from './utils/presets';
+import { usePreset } from './utils/usePreset';
+import { useState } from 'react';
 
 export default function PresetsTab() {
-  const loadPreset = useLoadPreset();
+  const { loadPreset, saveAsPreset } = usePreset();
+  const [presetString, setPresetString] = useState<string>('');
 
   return (
-    <List
-      bordered
-      dataSource={presets}
-      renderItem={(preset) => (
-        <List.Item>
-          <Button type="primary" onClick={() => loadPreset(preset)}>
-            {preset.name}
-          </Button>
-        </List.Item>
+    <Card
+      title="Presets"
+      style={{ height: '100%' }}
+      extra={
+        <Button
+          type="primary"
+          onClick={() => setPresetString(saveAsPreset('preset name'))}
+        >
+          Save Preset
+        </Button>
+      }
+      styles={{
+        body: {
+          padding: '1rem',
+        },
+      }}
+    >
+      {presetString !== '' && (
+        <Card inert>
+          <Form.Item
+            layout="vertical"
+            label={<h2 style={{ margin: 0 }}>Your preset is ready to be copied!</h2>}
+            help="Copy the text above and share it with others to load your preset."
+            style={{ marginBottom: 0 }}
+          >
+            <Input style={{ marginBottom: '0.3rem' }} value={presetString} />
+          </Form.Item>
+          <Divider />
+        </Card>
       )}
-    />
+      <List
+        dataSource={presets}
+        renderItem={(preset) => (
+          <List.Item>
+            <Button type="primary" onClick={() => loadPreset(preset)}>
+              {preset.name}
+            </Button>
+          </List.Item>
+        )}
+      />
+    </Card>
   );
 }
--- src/tabs/Presets/utils/usePreset.tsx
+++ src/tabs/Presets/utils/usePreset.tsx
@@ -1,15 +1,41 @@
 import { useCallback } from 'react';
-import { synthState } from '../../../state/Synth/synthState';
+import { synthState, SynthStateType } from '../../../state/Synth/synthState';
 import { Preset } from './presetTypes';
 import { effectChain } from '../../../driver/driver';
 import { effectButtons } from '../../Effects/utils/effectButtons';
 import { useEffectNodes } from '../../../context/EffectNodesContext/useEffectNodes';
 import { lfoState, LFOStateType } from '../../../state/LFO/lfoState';
-import { setSubsynthOpts } from '../../../state/Synth/subscribers/setters'
-import { eqState } from '../../../state/EQ/eqState'
+import { setSubsynthOpts } from '../../../state/Synth/subscribers/setters';
+import { eqState, EQStateType } from '../../../state/EQ/eqState';
+import { useSnapshot } from 'valtio';
+import { compressToBase64 } from 'lz-string'
 
-export const useLoadPreset = () => {
+export const usePreset = () => {
   const { addNode, addEdge, setEdges } = useEffectNodes();
+  const synthSnap = useSnapshot(synthState);
+  const lfoSnap = useSnapshot(lfoState);
+  const eqSnap = useSnapshot(eqState);
+
+  const saveAsPreset = useCallback(
+    (name: string) => {
+      const preset: Preset = {
+        name,
+        synthOpts: synthSnap as SynthStateType,
+        effects: [...effectChain.effects.values()].map((effect) => ({
+          id: effect.id,
+          effectName: effect.node.name,
+          options: effect.node.get() as unknown as Record<string, string | number>,
+          inputs: effect.inputs,
+          outputs: effect.outputs,
+        })),
+        lfos: lfoSnap as LFOStateType,
+        eq: eqSnap as EQStateType,
+      };
+
+      return compressToBase64(JSON.stringify(preset));
+    },
+    [synthSnap, lfoSnap, eqSnap]
+  );
 
   const loadPreset = useCallback(
     (preset: Preset) => {
@@ -81,5 +107,5 @@ export const useLoadPreset = () => {
     [addNode, addEdge, setEdges]
   );
 
-  return loadPreset;
+  return { loadPreset, saveAsPreset };
 };
